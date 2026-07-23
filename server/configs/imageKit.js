acfdcf50176817f88a30847b12b1a54e6a194866
abhaysingh-2005 <abhayrajawat787569@gmail.com>
2026-07-23 22:46:01 +0530

    Fix:

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,9 +1,26 @@
-import ImageKit from "imagekit";
+// import  ImageKit  from "imagekit";
+
+// const imagekit = new ImageKit({
+//     publicKey: process.env.IMAGEKIT_PUBLIC_KEY || 'public_adrKENdOZx0w18ZN4KkfN1m8vaI=',
+//     privateKey: process.env.IMAGEKIT_PRIVATE_KEY || 'private_StfAtc8mGefwcqXnrjxR6kYlPLI=',
+//     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/blogabhay'
+// });
+
+// export default imagekit;
+
+
+
+
+
+
+
+import pkg from 'imagekit';
+const  ImageKit  = pkg.default || pkg;;
 
 const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY || 'public_adrKENdOZx0w18ZN4KkfN1m8vaI=',
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY || 'private_StfAtc8mGefwcqXnrjxR6kYlPLI=',
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/blogabhay'
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/blogabhay' 
 });
 
 export default imagekit;
