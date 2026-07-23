59d711096102fa599f66c30cf1c593ba9913073f
abhaysingh-2005 <abhayrajawat787569@gmail.com>
2026-07-23 23:47:48 +0530

    FIX

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -14,13 +14,26 @@
 
 
 
+// import pkg from 'imagekit';
+// const  ImageKit  = pkg.default || pkg;;
+
+// const imagekit = new ImageKit({
+//     publicKey: process.env.IMAGEKIT_PUBLIC_KEY || 'public_adrKENdOZx0w18ZN4KkfN1m8vaI=',
+//     privateKey: process.env.IMAGEKIT_PRIVATE_KEY || 'private_StfAtc8mGefwcqXnrjxR6kYlPLI=',
+//     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/blogabhay' 
+// });
+
+// export default imagekit;
+
+
+
 import pkg from 'imagekit';
 const  ImageKit  = pkg.default || pkg;;
 
 const imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY || 'public_adrKENdOZx0w18ZN4KkfN1m8vaI=',
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY || 'private_StfAtc8mGefwcqXnrjxR6kYlPLI=',
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/blogabhay' 
+    publicKey:  'public_adrKENdOZx0w18ZN4KkfN1m8vaI=',
+    privateKey:  'private_StfAtc8mGefwcqXnrjxR6kYlPLI=',
+    urlEndpoint:  'https://ik.imagekit.io/blogabhay' 
 });
 
 export default imagekit;
